f881a8d207d3ef816bfc69e09c11a640af10e729
carol_rosa <caroline.rosa.silva@gmail.com>
2020-09-23 21:23:09 -0300

    Add signalR

--- Hubs/AppHub.cs
+++ Hubs/AppHub.cs
@@ -5,9 +5,19 @@ namespace MentorsBackEnd.Hubs
 {
     public class AppHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendNotificationLogin(string idCard)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("Login", idCard);
+        }
+
+        public async Task SendNotificationFinish(string message)
+        {
+            await Clients.All.SendAsync("Finish", message);
+        }
+
+        public async Task SendNotificationTeste(string message)
+        {
+            await Clients.All.SendAsync("Teste", message);
         }
     }
 }
